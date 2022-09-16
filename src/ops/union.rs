6863cf46d4c3ce4d04810d3803865daa89be31fe
Xophe <christophe.dousson@orange.com>
2022-09-16 10:35:35 +0200

    add time formatting + some tests

--- src/ops/union.rs
+++ src/ops/union.rs
@@ -2,9 +2,67 @@ use std::ops::{BitOr, BitOrAssign};
 use crate::*;
 use crate::iter::TimeUnion;
 
+//------------ TIME POINTS ------------
+
+
+impl<TW> BitOr<TW> for TimeValue
+    where TW: TimeConvex<TimePoint=TimeValue>
+{
+    type Output = TimeSet<TimeValue>;
+    #[inline] fn bitor(self, tw: TW) -> Self::Output { TimeInterval::singleton(self).bitor(tw) }
+}
+
+impl<TW> BitOr<TW> for Timestamp
+    where TW: TimeConvex<TimePoint=Timestamp>
+{
+    type Output = TimeSet<Timestamp>;
+    #[inline] fn bitor(self, tw: TW) -> Self::Output { TimeInterval::singleton(self).bitor(tw) }
+}
+
+//------------ TIME INTERVALS ------------
+
+
+impl<T:TimePoint,TW> BitOr<TW> for TimeInterval<T>
+    where TW: TimeConvex<TimePoint=T>
+{
+    type Output = TimeSet<T>;
+    #[inline] fn bitor(self, tw: TW) -> Self::Output { (&self).bitor(tw) }
+}
+
+impl<T:TimePoint,TW> BitOr<TW> for &TimeInterval<T>
+    where TW: TimeConvex<TimePoint=T>
+{
+    type Output = TimeSet<T>;
+
+    fn bitor(self, tw: TW) -> Self::Output
+    {
+        if self.is_empty() {
+            TimeSet::from(tw)
+        } else {
+            if tw.is_empty() {
+                (*self).into()
+            } else {
+                if tw.upper_bound() < self.lower.just_before() {
+                    TimeSet(vec![tw.into(), *self])
+                } else if self.upper < tw.lower_bound().just_before() {
+                    TimeSet(vec![*self, tw.into()])
+                } else {
+                    TimeSet(vec![
+                        TimeInterval {
+                            lower: self.lower.min(tw.lower_bound()),
+                            upper: self.upper.max(tw.upper_bound())
+                        }
+                    ])
+                }
+            }
+        }
+    }
+}
+
+//------------ TIME SETS ------------
 
 impl<T:TimePoint,TW> BitOrAssign<TW> for TimeSet<T>
-    where TW: Into<TimeInterval<T>>
+    where TW: TimeConvex<TimePoint=T>
 {
     fn bitor_assign(&mut self, tw: TW) {
         // todo: optimise cloning
@@ -28,14 +86,6 @@ impl<T:TimePoint> BitOrAssign<&Self> for TimeSet<T>
     }
 }
 
-impl<T:TimePoint,TW> BitOr<TW> for TimeInterval<T>
-    where TW: Into<TimeInterval<T>>
-{
-    type Output = TimeSet<T>;
-    #[inline] fn bitor(self, tw: TW) -> Self::Output { (&self).bitor(tw) }
-}
-
-
 impl<T:TimePoint> BitOr<Self> for TimeSet<T>
 {
     type Output = Self;
@@ -49,7 +99,7 @@ impl<T:TimePoint> BitOr<&Self> for TimeSet<T>
 }
 
 impl<T:TimePoint, TW> BitOr<TW> for TimeSet<T>
-    where TW: Into<TimeInterval<T>>
+    where TW: TimeConvex<TimePoint=T>
 {
     type Output = Self;
     #[inline] fn bitor(self, tw: TW) -> Self::Output { (&self).bitor(tw) }
@@ -78,7 +128,7 @@ impl<T:TimePoint> BitOr<Self> for &TimeSet<T>
 
 
 impl<T:TimePoint, TW> BitOr<TW> for &TimeSet<T>
-    where TW: Into<TimeInterval<T>>
+    where TW: TimeConvex<TimePoint=T>
 {
     type Output = TimeSet<T>;
 
@@ -88,38 +138,3 @@ impl<T:TimePoint, TW> BitOr<TW> for &TimeSet<T>
     }
 }
 
-
-impl<T:TimePoint,TW> BitOr<TW> for &TimeInterval<T>
-    where TW: Into<TimeInterval<T>>
-{
-    type Output = TimeSet<T>;
-
-    fn bitor(self, tw: TW) -> Self::Output
-    {
-        let i = tw.into();
-        if self.is_empty() {
-            if i.is_empty() {
-                TimeSet::empty()
-            } else {
-                i.into()
-            }
-        } else {
-            if i.is_empty() {
-                (*self).into()
-            } else {
-                if i.upper < self.lower.just_before() {
-                    TimeSet(vec![i, *self])
-                } else if self.upper < i.lower.just_before() {
-                    TimeSet(vec![*self, i])
-                } else {
-                    TimeSet(vec![
-                        TimeInterval {
-                            lower: self.lower.min(i.lower),
-                            upper: self.upper.max(i.upper)
-                        }
-                    ])
-                }
-            }
-        }
-    }
-}
